b06af17312d6e7fb10cd5aa0c35d6ddb238cc884
HueyPark <jaewan.huey.park@gmail.com>
2016-10-17 08:36:31 +0900

    added physics function add_torque

--- Source/UnrealEnginePython/Private/UEPyPhysics.h
+++ Source/UnrealEnginePython/Private/UEPyPhysics.h
@@ -5,4 +5,5 @@
 #include "UnrealEnginePython.h"
 
 PyObject *py_ue_set_simulate_physics(ue_PyUObject *, PyObject *);
+PyObject *py_ue_add_torque(ue_PyUObject *, PyObject *);
 PyObject *py_ue_destructible_apply_damage(ue_PyUObject *, PyObject *);
