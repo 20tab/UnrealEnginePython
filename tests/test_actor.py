40ab90d9929bd6f3a3844b04f5dbca8c85e4ffc5
Roberto De Ioris <roberto@20tab.com>
2017-02-12 13:39:53 +0100

    another round of unit tests

---
+++ tests/test_actor.py
@@ -0,0 +1,48 @@
+import unittest
+import unreal_engine as ue
+from unreal_engine.classes import Actor, Character
+from unreal_engine import FVector, FRotator
+import time
+import math
+
+class TestActor(unittest.TestCase):
+
+    def setUp(self):
+    	self.world = ue.get_editor_world()
+
+    def test_spawn(self):
+    	new_actor = self.world.actor_spawn(Actor)
+    	actor_label = 'ActorTest_' + str(time.time())
+    	new_actor.set_actor_label(actor_label)
+    	found_actor = self.world.find_actor_by_label(actor_label)
+    	self.assertEqual(found_actor, new_actor)
+
+    def test_location(self):
+    	new_actor = self.world.actor_spawn(Character, FVector(100, 200, 300))
+    	self.assertEqual(new_actor.get_actor_location(), FVector(100, 200, 300))
+
+    def test_rotation(self):
+    	new_actor = self.world.actor_spawn(Character, FVector(), FRotator(0, 0, 90))
+    	yaw = new_actor.get_actor_rotation().yaw
+    	self.assertTrue(math.fabs(90 - yaw) < 0.1)
+
+    def test_is_a(self):
+    	new_actor = self.world.actor_spawn(Character, FVector(100, 200, 300))
+    	self.assertTrue(new_actor.is_a(Actor))
+    	self.assertTrue(new_actor.is_a(Character))
+    	new_actor2 = self.world.actor_spawn(Actor, FVector(100, 200, 300))
+    	self.assertTrue(new_actor2.is_a(Actor))
+    	self.assertFalse(new_actor2.is_a(Character))
+
+    def test_destroy(self):
+    	new_actor = self.world.actor_spawn(Actor)
+    	actor_label = 'ActorDestroyTest_' + str(time.time())
+    	new_actor.set_actor_label(actor_label)
+    	found_actor = self.world.find_actor_by_label(actor_label)
+    	self.assertEqual(found_actor, new_actor)
+    	new_actor.actor_destroy()
+    	found_actor = self.world.find_actor_by_label(actor_label)
+    	self.assertIsNone(found_actor)
+
+
+
---
+++ tests/test_uobject.py
@@ -0,0 +1,23 @@
+import unittest
+import unreal_engine as ue
+from unreal_engine.classes import Material, Character
+from unreal_engine import FVector, FRotator
+import time
+import math
+
+class TestUObject(unittest.TestCase):
+
+    def test_new_object(self):
+        new_material = Material()
+        self.assertIsNotNone(new_material)
+
+    def test_set_name(self):
+        new_material = Material()
+        mat_name = 'MatTest_' + str(time.time())
+        new_material.set_name(mat_name)
+        self.assertEqual(new_material.get_name(), mat_name)
+
+   
+
+
+
