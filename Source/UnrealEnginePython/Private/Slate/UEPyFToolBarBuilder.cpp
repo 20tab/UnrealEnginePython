7160dfc0cb6abf6745aef1d27d07b4193042b264
rdeioris <roberto@20tab.com>
2017-05-29 11:19:30 +0200

    aaded missing FToolbarBuilder files

---
+++ Source/UnrealEnginePython/Private/Slate/UEPyFToolBarBuilder.cpp
@@ -0,0 +1,168 @@
+#include "UnrealEnginePythonPrivatePCH.h"
+
+#include "UEPyFToolBarBuilder.h"
+
+#include "Runtime/Slate/Public/Framework/Commands/UIAction.h"
+
+static PyObject *py_ue_ftool_bar_builder_begin_section(ue_PyFToolBarBuilder *self, PyObject * args) {
+	char *name;
+	if (!PyArg_ParseTuple(args, "s:begin_section", &name))
+		return NULL;
+
+	self->tool_bar_builder->BeginSection(FName(name));
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyObject *py_ue_ftool_bar_builder_end_section(ue_PyFToolBarBuilder *self, PyObject * args) {
+	self->tool_bar_builder->EndSection();
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyObject *py_ue_ftool_bar_builder_add_tool_bar_button(ue_PyFToolBarBuilder *self, PyObject * args) {
+	char *hook;
+	char *label;
+	char *tooltip;
+	PyObject *py_callable;
+	PyObject *py_obj = nullptr;
+	if (!PyArg_ParseTuple(args, "sssO|O:add_tool_bar_button", &hook, &label, &tooltip, &py_callable, &py_obj))
+		return NULL;
+
+	if (!PyCallable_Check(py_callable)) {
+		return PyErr_Format(PyExc_Exception, "argument is not callable");
+	}
+
+	FExecuteAction handler;
+	UPythonSlateDelegate *py_delegate = NewObject<UPythonSlateDelegate>();
+	py_delegate->SetPyCallable(py_callable);
+	py_delegate->AddToRoot();
+
+	if (py_obj) {
+		Py_INCREF(py_obj);
+		handler.BindUObject(py_delegate, &UPythonSlateDelegate::ExecuteAction, py_obj);
+	}
+	else {
+		handler.BindUObject(py_delegate, &UPythonSlateDelegate::SimpleExecuteAction);
+	}
+
+	self->tool_bar_builder->AddToolBarButton(FUIAction(handler), FName(hook), FText::FromString(UTF8_TO_TCHAR(label)), FText::FromString(UTF8_TO_TCHAR(tooltip)), FSlateIcon());
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyObject *py_ue_ftool_bar_builder_add_separator(ue_PyFToolBarBuilder *self, PyObject * args) {
+	char *hook = nullptr;
+
+	if (!PyArg_ParseTuple(args, "|s:add_separator", &hook))
+		return NULL;
+
+	FName f_name = NAME_None;
+
+	if (hook)
+		f_name = FName(UTF8_TO_TCHAR(hook));
+
+	self->tool_bar_builder->AddSeparator(f_name);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+static PyObject *py_ue_ftool_bar_builder_begin_block_group(ue_PyFToolBarBuilder *self, PyObject * args) {
+	self->tool_bar_builder->BeginBlockGroup();
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyObject *py_ue_ftool_bar_builder_end_block_group(ue_PyFToolBarBuilder *self, PyObject * args) {
+	self->tool_bar_builder->EndBlockGroup();
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyMethodDef ue_PyFToolBarBuilder_methods[] = {
+	{ "begin_section", (PyCFunction)py_ue_ftool_bar_builder_begin_section, METH_VARARGS, "" },
+	{ "end_section", (PyCFunction)py_ue_ftool_bar_builder_end_section, METH_VARARGS, "" },
+	{ "add_tool_bar_button", (PyCFunction)py_ue_ftool_bar_builder_add_tool_bar_button, METH_VARARGS, "" },
+	{ "add_separator", (PyCFunction)py_ue_ftool_bar_builder_add_separator, METH_VARARGS, "" },
+	{ "begin_block_group", (PyCFunction)py_ue_ftool_bar_builder_begin_block_group, METH_VARARGS, "" },
+	{ "end_block_group", (PyCFunction)py_ue_ftool_bar_builder_end_block_group, METH_VARARGS, "" },
+	{ NULL }  /* Sentinel */
+};
+
+
+static PyObject *ue_PyFToolBarBuilder_str(ue_PyFToolBarBuilder *self)
+{
+	return PyUnicode_FromFormat("<unreal_engine.FToolBarBuilder '%p'}>",
+		&self->tool_bar_builder);
+}
+
+static void ue_py_ftool_bar_builder_dealloc(ue_PyFToolBarBuilder *self) {
+	delete(self->tool_bar_builder);
+#if PY_MAJOR_VERSION < 3
+	self->ob_type->tp_free((PyObject*)self);
+#else
+	Py_TYPE(self)->tp_free((PyObject*)self);
+#endif
+}
+
+static PyTypeObject ue_PyFToolBarBuilderType = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	"unreal_engine.FToolBarBuilder", /* tp_name */
+	sizeof(ue_PyFToolBarBuilder), /* tp_basicsize */
+	0,                         /* tp_itemsize */
+	(destructor)ue_py_ftool_bar_builder_dealloc,       /* tp_dealloc */
+	0,                         /* tp_print */
+	0,                         /* tp_getattr */
+	0,                         /* tp_setattr */
+	0,                         /* tp_reserved */
+	0,                         /* tp_repr */
+	0,                         /* tp_as_number */
+	0,                         /* tp_as_sequence */
+	0,                         /* tp_as_mapping */
+	0,                         /* tp_hash  */
+	0,                         /* tp_call */
+	(reprfunc)ue_PyFToolBarBuilder_str,                         /* tp_str */
+	0,                         /* tp_getattro */
+	0,                         /* tp_setattro */
+	0,                         /* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,        /* tp_flags */
+	"Unreal Engine FToolBarBuilder",           /* tp_doc */
+	0,                         /* tp_traverse */
+	0,                         /* tp_clear */
+	0,                         /* tp_richcompare */
+	0,                         /* tp_weaklistoffset */
+	0,                         /* tp_iter */
+	0,                         /* tp_iternext */
+	ue_PyFToolBarBuilder_methods,             /* tp_methods */
+};
+
+static int ue_py_ftool_bar_builder_init(ue_PyFToolBarBuilder *self, PyObject *args, PyObject *kwargs) {
+	PyErr_SetString(PyExc_ValueError, "manually instantiating a new FToolBarBuilder is not supported");
+	return -1;
+}
+
+
+void ue_python_init_ftool_bar_builder(PyObject *ue_module) {
+	ue_PyFToolBarBuilderType.tp_new = PyType_GenericNew;
+
+	ue_PyFToolBarBuilderType.tp_init = (initproc)ue_py_ftool_bar_builder_init;
+
+	if (PyType_Ready(&ue_PyFToolBarBuilderType) < 0)
+		return;
+
+	Py_INCREF(&ue_PyFToolBarBuilderType);
+	PyModule_AddObject(ue_module, "FToolBarBuilder", (PyObject *)&ue_PyFToolBarBuilderType);
+}
+
+PyObject *py_ue_new_ftool_bar_builder(FToolBarBuilder *tool_bar_builder) {
+	ue_PyFToolBarBuilder *ret = (ue_PyFToolBarBuilder *)PyObject_New(ue_PyFToolBarBuilder, &ue_PyFToolBarBuilderType);
+	ret->tool_bar_builder = tool_bar_builder;
+	return (PyObject *)ret;
+}
---
+++ Source/UnrealEnginePython/Private/Slate/UEPyFToolBarBuilder.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "UnrealEnginePython.h"
+
+#include "Runtime/Slate/Public/Framework/MultiBox/MultiBoxBuilder.h"
+
+typedef struct {
+	PyObject_HEAD
+	/* Type-specific fields go here. */
+	FToolBarBuilder *tool_bar_builder;
+} ue_PyFToolBarBuilder;
+
+void ue_python_init_ftool_bar_builder(PyObject *);
+
+PyObject *py_ue_new_ftool_bar_builder(FToolBarBuilder *);
