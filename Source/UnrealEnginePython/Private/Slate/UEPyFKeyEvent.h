063a4668e50c54c025f59f9bae5843ee15545855
Roberto De Ioris <roberto@20tab.com>
2017-08-23 14:38:30 +0200

    added key event support for slate

---
+++ Source/UnrealEnginePython/Private/Slate/UEPyFKeyEvent.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "UnrealEnginePython.h"
+
+#include "UEPyFInputEvent.h"
+
+extern PyTypeObject ue_PyFInputEventType;
+
+typedef struct {
+	/* Type-specific fields go here. */
+	ue_PyFInputEvent f_input;
+	FKeyEvent key_event;
+} ue_PyFKeyEvent;
+
+void ue_python_init_fkey_event(PyObject *);
+
+PyObject *py_ue_new_fkey_event(FKeyEvent);
