b14cf86afcab73bef02354f526c46ddfcc999e81
Roberto De Ioris <roberto@20tab.com>
2017-09-01 10:33:08 +0200

    added get_blueprint_components

--- Source/UnrealEnginePython/Private/UEPyModule.cpp
+++ Source/UnrealEnginePython/Private/UEPyModule.cpp
@@ -255,6 +255,7 @@ static PyMethodDef unreal_engine_methods[] = {
 	{ "blueprint_add_ubergraph_page", py_unreal_engine_blueprint_add_ubergraph_page, METH_VARARGS, "" },
 	{ "blueprint_mark_as_structurally_modified", py_unreal_engine_blueprint_mark_as_structurally_modified, METH_VARARGS, "" },
 	{ "add_component_to_blueprint", py_unreal_engine_add_component_to_blueprint, METH_VARARGS, "" },
+	{ "get_blueprint_components", py_unreal_engine_get_blueprint_components, METH_VARARGS, "" },
 	{ "create_material_instance", py_unreal_engine_create_material_instance, METH_VARARGS, "" },
 	{ "message_dialog_open", py_unreal_engine_message_dialog_open, METH_VARARGS, "" },
 	{ "create_modal_save_asset_dialog", py_unreal_engine_create_modal_save_asset_dialog, METH_VARARGS, "" },
