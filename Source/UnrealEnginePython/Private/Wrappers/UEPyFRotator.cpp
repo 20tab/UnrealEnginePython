db120974b318c2fc6c223427e6caa0309219aaef
ikrima <contact@ikrima.com>
2018-02-19 23:52:18 -0800

    FIX: Bug with order of FRotator initialization

--- Source/UnrealEnginePython/Private/Wrappers/UEPyFRotator.cpp
+++ Source/UnrealEnginePython/Private/Wrappers/UEPyFRotator.cpp
@@ -225,25 +225,30 @@ PySequenceMethods ue_PyFRotator_sequence_methods;
 static int ue_py_frotator_init(ue_PyFRotator *self, PyObject *args, PyObject *kwargs) {
 	float pitch = 0, yaw = 0, roll = 0;
     
-	if (!PyArg_ParseTuple(args, "|fff", &roll, &pitch, &yaw))
-		return -1;
-
-
-    ue_py_uscriptstruct_alloc(&self->py_base, TBaseStructure<FRotator>::Get(), nullptr, false);
-
-    if (ue_PyFQuat *py_quat = PyTuple_Size(args) == 1 ? py_ue_is_fquat(PyTuple_GetItem(args, 0)) : nullptr) {
-        py_ue_frotator_get(self) = FRotator(py_ue_fquat_get(py_quat));
-        return 0;
+    if (PyTuple_Size(args) == 3 && PyArg_ParseTuple(args, "|fff", &roll, &pitch, &yaw))
+    {
     }
-
-	if (PyTuple_Size(args) == 1) {
+    else if (PyTuple_Size(args) == 1 && PyArg_ParseTuple(args, "|f", &roll))
+    {
         pitch = roll;
         yaw   = roll;
-	}
+    }
+    else if (ue_PyFQuat *py_quat = PyTuple_Size(args) == 1 ? py_ue_is_fquat(PyTuple_GetItem(args, 0)) : nullptr) 
+    {
+        FRotator rot_from_quat = FRotator(py_ue_fquat_get(py_quat));
+        roll  = rot_from_quat.Roll;
+        pitch = rot_from_quat.Pitch;
+        yaw   = rot_from_quat.Yaw;
+    }
+    else
+    {
+        return -1;
+    }
 
+    ue_py_uscriptstruct_alloc(&self->py_base, TBaseStructure<FRotator>::Get(), nullptr, false);
 	py_ue_frotator_get(self).Pitch = pitch;
-	py_ue_frotator_get(self).Yaw = yaw;
-	py_ue_frotator_get(self).Roll = roll;
+	py_ue_frotator_get(self).Yaw   = yaw;
+	py_ue_frotator_get(self).Roll  = roll;
 
 	return 0;
 }
