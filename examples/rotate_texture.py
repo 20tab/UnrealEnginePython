9d857920f8b8285871f88a9546b0c4f0ac193af6
Roberto De Ioris <roberto@20tab.com>
2018-06-30 12:49:40 +0200

    Update rotate_texture.py

--- examples/rotate_texture.py
+++ examples/rotate_texture.py
@@ -5,11 +5,17 @@ from unreal_engine.classes import Texture2D
 def rotate_selected_assets_data(degrees):
     for asset in ue.get_selected_assets():
         if asset.is_a(Texture2D):
-            tex = ue.load_object(Texture2D, asset.get_path_name())
-            data = tex.texture_get_data()
-            size = (asset.Blueprint_GetSizeX(), asset.Blueprint_GetSizeY())
+            data = asset.texture_get_source_data()
+            
+            size = (asset.texture_get_width(), asset.texture_get_height())
+           
+            img = Image.frombytes('RGBA', size, bytes(data))
+            
+            out = img.rotate(degrees)
 
-            img = Image.frombytes("RGB", size, data)
-            img.show()
-            out = img.rotate(degrees, expand=1)
-            tex.texture_set_data(out.tobytes())
+            asset.texture_set_source_data(out.tobytes())
+
+            
+            
+
+rotate_selected_assets_data(45)
