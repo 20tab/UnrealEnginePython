62016f5eaa0d7f8481d7943b4c15aa1b119fee37
Roberto De Ioris <roberto.deioris@gmail.com>
2019-04-23 16:23:45 +0200

    fix for 4.22

--- Source/UnrealEnginePython/Private/UObject/UEPyWorld.cpp
+++ Source/UnrealEnginePython/Private/UObject/UEPyWorld.cpp
@@ -316,7 +316,7 @@ PyObject *py_ue_set_current_level(ue_PyUObject *self, PyObject * args)
 	if (!level)
 		return PyErr_Format(PyExc_Exception, "argument is not a ULevel");
 
-#if WITH_EDITOR
+#if WITH_EDITOR || ENGINE_MINOR_VERSION < 22
 
 	if (world->SetCurrentLevel(level))
 		Py_RETURN_TRUE;
